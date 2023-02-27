fa0eb389f43891debf94a46647c917ef24de4a03
Hector Eduardo Aguirre Santiago <eduardo.aguirre@outlook.com>
2023-02-27 12:20:41 -0600

    Adding new method

--- sonarqube-scanner-maven/maven-basic/src/main/java/com/acme/basic/HelloWorld.java
+++ sonarqube-scanner-maven/maven-basic/src/main/java/com/acme/basic/HelloWorld.java
@@ -9,5 +9,9 @@ public class HelloWorld {
   void notCovered() {
     System.out.println("This method is not covered by unit tests");
   }
+  
+  void notCovered2(){
+    System.out.println("New method without covered unit tests");
+  }
 
 }
